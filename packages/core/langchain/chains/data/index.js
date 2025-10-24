0f3652f2fe5f0dc3a8c9fae890780a0ad84af834
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-10-25 00:01:19 +0200

    many imports

--- packages/core/langchain/chains/data/index.js
+++ packages/core/langchain/chains/data/index.js
@@ -1,19 +1,42 @@
 import { RunnableSequence, RunnablePassthrough } from "@langchain/core/runnables";
 import { retriever } from "../../../supabase/retriever/data";
-import { standAloneQuestionPrompt } from "@csbot/templates";
+import { standAloneQuestionTemplate } from "@csbot/templates";
 import { StringOutputParser } from "@langchain/core/output_parsers";
+import { combineDocuments } from "../../../utils";
+import { BufferMemory } from "langchain/memory";
+
+const memory = new BufferMemory({
+  memoryKey: "history",
+  returnMessages: true,
+  inputKey: "question",
+});
 
 //tar en array
 const standAloneQuestionChain = RunnableSequence.from([
-  standAloneQuestionPrompt, //hämtar min prompt för att få specifik fråga
+  standAloneQuestionTemplate, //hämtar min prompt för att få specifik fråga
   llm, //skickar den till min språkmodell, som producerar ett svar
   new StringOutputParser(), //plockar ut endast sträng
 ]);
 
+const retrieverChain = RunnableSequence.from([
+  (data) => {
+    return data.standaloneQuestion;
+  },
+  retriever,
+  combineDocuments,
+]);
+
+const conversationChain = new conversationChain({
+  llm,
+  prompt: answerTemplate,
+  memory,
+});
+
 export const chain = RunnableSequence.from([
   {
-    originalQuestion: new RunnablePassthrough(),
     standaloneQuestion: standAloneQuestionChain,
+    originalQuestion: new RunnablePassthrough(),
   },
-  {},
+  { context: retrieverChain, question: ({ originalQuestion }) => originalQuestion.question },
+  conversationChain,
 ]);
