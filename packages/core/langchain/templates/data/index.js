96ed8473510e0f7c77ca30b469fdee1ffea5d196
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-10-25 00:01:40 +0200

    export const answerTemplate

--- packages/core/langchain/templates/data/index.js
+++ packages/core/langchain/templates/data/index.js
@@ -1,6 +1,17 @@
 import { PromptTemplate, ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
-export const standAloneQuestionPrompt = PromptTemplate.fromTemplate(`
+export const standAloneQuestionTemplate = PromptTemplate.fromTemplate(`
     Givet en fråga om företaget TechNova AB. Gör om den frågan till en standalone question som är tydlig och enkel att förstå.
     Fråga: question
     Standalone Question:
     `);
+
+export const answerTemplate = ChatPromptTemplate.fromMessages([
+  ["system", `Du vet allt om företaget TechNova AB. Du är tydlig och`],
+  new MessagesPlaceholder("history"),
+  [
+    "user",
+    `Kontext: {context}
+    Fråga: {question} 
+    Svar:`,
+  ],
+]);
