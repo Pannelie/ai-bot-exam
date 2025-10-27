7c172d9c5a42a11c698fee53b1f170f3c1f5ac8b
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-10-27 12:11:55 +0100

    created homepage

---
+++ packages/pages/homepage/ui/index.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+export const HomePage = () => {
+  return (
+    <main className="home-page">
+      <header className="header"></header>
+
+      <article className="hero">
+        <h1 className="hero__title">Hero</h1>
+        <p className="hero__text">Hero text blablabla</p>
+      </article>
+    </main>
+  );
+};
