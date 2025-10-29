11c582e35e86700dac0c46645969dfd0199197bb
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-10-29 14:25:43 +0100

    changed into swedish

--- packages/base/popupbtn/ui/index.jsx
+++ packages/base/popupbtn/ui/index.jsx
@@ -14,10 +14,17 @@ export const PopUpBtn = () => {
       <div className="popup-button" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} onClick={toggle}>
         {!open && (
           <>
-            <Logo type="popup" /> <div className={`tooltip-text ${hover ? "visible" : ""}`}>Behöver du hjälp?</div>{" "}
+            <Logo type="popup" aria-label="Öppna chatt för hjälp" aria-describedby="tooltip-chat" />{" "}
+            <div id="tooltip-chat" className={`tooltip-text ${hover ? "visible" : ""}`}>
+              Behöver du hjälp?
+            </div>{" "}
           </>
         )}
-        {open && <button className={"close-btn"}>✕</button>}
+        {open && (
+          <button className={"close-btn"} aria-label="Stäng chat">
+            ✕
+          </button>
+        )}
       </div>
     </div>
   );
