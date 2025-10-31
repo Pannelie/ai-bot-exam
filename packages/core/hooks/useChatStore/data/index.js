3be524cd0fb9011e6cd92daee480b82b0c487876
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-10-31 11:09:34 +0100

    missed =>

--- packages/core/hooks/useChatStore/data/index.js
+++ packages/core/hooks/useChatStore/data/index.js
@@ -4,11 +4,9 @@ export const useChatStore = create((set) => ({
   messages: [],
   addMessage: (msg) => set((state) => ({ messages: [...state.messages, msg] })),
   updateMessage: (id, updatedFields) => {
-    set(
-      state({
-        messages: state.messages.map((msg) => (msg.id === id ? { ...msg, ...updatedFields } : msg)),
-      })
-    );
+    set((state) => ({
+      messages: state.messages.map((msg) => (msg.id === id ? { ...msg, ...updatedFields } : msg)),
+    }));
   },
   clearMessages: () => set({ messages: [] }),
 }));
