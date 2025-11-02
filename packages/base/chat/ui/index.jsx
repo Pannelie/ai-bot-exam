fc00080d6b0976c8af0760a1053d66d811d2dcc5
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-11-02 14:49:24 +0100

    simplified my messageComponents to only include onSourcClick when there is a souce to be clicked on

--- packages/base/chat/ui/index.jsx
+++ packages/base/chat/ui/index.jsx
@@ -7,32 +7,17 @@ import { Header } from "@csbot/header";
 export const Chat = ({ onSourceClick }) => {
   const { messages, handleSubmit, inputRef } = useAskQuestion();
 
-  // const messageComponents = messages.map((message, index) => {
-  //   if (message.source && onSourceClick) {
-  //     return (
-  //       <Message
-  //         key={message.id || index}
-  //         role={message.role}
-  //         text={message.content}
-  //         loading={message.loading}
-  //         source={message.source}
-  //         onSourceClick={message.source && onSourceClick ? () => onSourceClick(message.highlight) : undefined}
-  //       />
-  //     );
-  //   }
-  //   return <Message key={index} role={message.role} text={message.content} />;
-  // });
-
   const messageComponents = messages.map((message, index) => (
     <Message
-      key={message.id || index}
+      key={index}
       role={message.role}
       text={message.content}
       loading={message.loading}
       source={message.source}
-      onSourceClick={message.source && onSourceClick ? () => onSourceClick(message.highlight) : undefined}
+      onSourceClick={message.source && onSourceClick ? () => onSourceClick(message.source) : undefined}
     />
   ));
+
   return (
     <section className="chat">
       <Header title={"Nova"} type={"small"} />
